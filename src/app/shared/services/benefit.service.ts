8f558e806178a6a3e161f4e514bab4dc397a6355
Riza Alif Wildani <rizaalifdmc@gmail.com>
2019-03-15 16:19:39 +0700

    Fixed master benefit

---
+++ src/app/shared/services/benefit.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { LocalStoreService } from "./local-store.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class BenefitService {
+  readonly apiURL = "http://45.118.132.77/api";
+  // readonly apiURL = "http://localhost/efc/api";
+
+  constructor(private http: HttpClient, private store: LocalStoreService) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: "Bearer " + this.store.getItem("access_token")
+    })
+  };
+
+  getBenefits() {
+    return this.http.get(this.apiURL + "/benefits", this.httpOptions);
+  }
+
+  createBenefit(data) {
+    return this.http.post(this.apiURL + "/benefits", data, this.httpOptions);
+  }
+
+  deleteBenefit(id) {
+    return this.http.delete(this.apiURL + "/benefits/" + id, this.httpOptions);
+  }
+
+  showBenefit(id) {
+    return this.http.get(this.apiURL + "/benefits/" + id, this.httpOptions);
+  }
+
+  updateBenefit(id, data) {
+    return this.http.put(
+      this.apiURL + "/benefits/" + id,
+      data,
+      this.httpOptions
+    );
+  }
+}
