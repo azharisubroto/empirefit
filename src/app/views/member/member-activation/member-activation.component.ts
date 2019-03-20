c269f44927a764c26b8dbbbb738807e5d50922b1
Riza Alif Wildani <rizaalifdmc@gmail.com>
2019-03-20 20:21:54 +0700

    Build Prod

--- src/app/views/member/member-activation/member-activation.component.ts
+++ src/app/views/member/member-activation/member-activation.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CustomValidators } from 'ng2-validation';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from "@angular/core";
+import { CustomValidators } from "ng2-validation";
+import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
 import { MemberService } from "src/app/shared/services/member.service";
 import { MemberTypeService } from "src/app/shared/services/member-type.service";
 import { Router, ActivatedRoute } from "@angular/router";
 import { PackageService } from "src/app/shared/services/package.service";
 import { BankService } from "src/app/shared/services/bank.service";
-import { PersonaltrainerService } from 'src/app/shared/services/personaltrainer.service';
-
+import { PersonaltrainerService } from "src/app/shared/services/personaltrainer.service";
 
 @Component({
-  selector: 'app-member-activation',
-  templateUrl: './member-activation.component.html',
-  styleUrls: ['./member-activation.component.scss']
+  selector: "app-member-activation",
+  templateUrl: "./member-activation.component.html",
+  styleUrls: ["./member-activation.component.scss"]
 })
 export class MemberActivationComponent implements OnInit {
   isCompleted: boolean;
   data: any = {
-    email: ''
+    email: ""
   };
   step2Form: FormGroup;
   loading: boolean;
@@ -40,11 +39,11 @@ export class MemberActivationComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private bank: BankService,
     private PersonalTrainer: PersonaltrainerService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.member = {make: ""};
-    this.expirydate = {make: ""};
+    this.member = { make: "" };
+    this.expirydate = { make: "" };
     this.radioGroup = this.fb.group({
       radio: []
     });
@@ -53,40 +52,42 @@ export class MemberActivationComponent implements OnInit {
     });
 
     // get user data
-    this.memberService.getSingleMember( this.activatedRoute.snapshot.params["id"] ).subscribe((data: any) => {
-      this.member = data["data"];
-      var date = new Date(data["data"]["expairy_date"]);
-      var list = date.toUTCString().split( " ")
-      //results.push(list[1]+" "+list[2]);
-      this.expirydate = list[1] + " " + list[2] + " " + list[3];
-      console.log( this.member ); 
-    });
+    this.memberService
+      .getSingleMember(this.activatedRoute.snapshot.params["id"])
+      .subscribe((data: any) => {
+        this.member = data["data"];
+        var date = new Date(data["data"]["expairy_date"]);
+        var list = date.toUTCString().split(" ");
+        //results.push(list[1]+" "+list[2]);
+        this.expirydate = list[1] + " " + list[2] + " " + list[3];
+        console.log(this.member);
+      });
 
     // get member type
     this.MemberTypeService.getMemberTypes().subscribe((data: any) => {
       this.type = data["data"];
-      console.log( this.type ); 
+      console.log(this.type);
     });
 
     this.PackageService.getPackages().subscribe((data: any) => {
       this.package = data["data"];
-      console.log( this.package ); 
+      console.log(this.package);
     });
 
     this.bank.getBanks().subscribe((data: any) => {
       this.banks = data["data"];
-      console.log( this.banks ); 
+      console.log(this.banks);
     });
 
     this.PersonalTrainer.getPersonalTrainers().subscribe((data: any) => {
       this.trainers = data["data"];
-      console.log( this.trainers ); 
+      console.log(this.trainers);
     });
-
   }
 
   onStep1Next(e) {}
   onStep2Next(e) {}
   onStep3Next(e) {}
+  onStep4Next(e) {}
   onComplete(e) {}
 }
