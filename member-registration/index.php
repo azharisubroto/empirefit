33f1f608d88368c5e34b388c2660ed4057079808
Azhari Subroto <juarathemes@gmail.com>
2019-03-25 18:51:58 +0700

    static member registration

---
+++ member-registration/index.php
@@ -0,0 +1,178 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Empire Fit | Online Registration</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    
+    <header class="py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-1 col-2">
+                    <img src="images/logo.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-11 col-10 text-right">
+                    <h3>Online Registration</h3>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="pb-5">
+        <div class="container">
+            <form action="submit.php">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="row mb-4">
+                            <div class="col-12">
+                                <h4>Biodata</h4>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="form-group col-md-6 mb-4">
+                                        <label for="firstName">Name</label>
+                                        <input type="text" class="form-control" name="firstName" placeholder="First">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Email Address">
+                                    </div>
+                                    <div class="form-group col-md-6 mb-4">
+                                        <label for="lastName">Mobile Phone Number</label>
+                                        <input type="text" class="form-control" id="mobilephone" name="mobilephone" placeholder="Mobile Phone">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="phone">ID Card (KTP/SIM/)</label>
+                                        <input type="text" class="form-control" id="idcard" name="idcard" placeholder="ID Card">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row my-4">
+                            <div class="col-12">
+                                <h4>Agreement</h4>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h6 class="mb-2 fancy-head"><span>Personal Data of Participants</span></h6>
+                                    </div>
+                                    <div class="form-group col-md-6 mb-4">
+                                        <label for="firstName">Address</label>
+                                        <input type="text" class="form-control" name="address" placeholder="Address" >
+                                    </div>
+                                    <div class="form-group col-md-6 mb-4">
+                                        <label for="firstName">Date of Birth</label>
+                                        <input type="text" class="form-control" name="datebirht" placeholder="Date of Birth" >
+                                    </div>
+                                    <div class="form-group col-md-6 mb-4">
+                                        <label for="firstName">Contact Number:</label>
+                                        <input type="text" class="form-control" name="contactnumber" placeholder="Contact Number" >
+                                    </div>
+                                    <div class="form-group col-md-6 mb-4">
+                                        <label for="firstName">In an emergency, I would like EMPIRE FIT CLUB to Call:</label>
+                                        <input type="text" class="form-control" name="emergency" placeholder="Emergency Number" >
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-12 mb-3">
+                                        <h6 class="mb-2 fancy-head"><span>Health Questions</span></h6>
+                                    </div>
+                                    
+                                    <div class="col-12">
+                                        <div class="questions"></div>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-12 mb-3">
+                                        <h6 class="mb-2 fancy-head"><span>Reviewer Note</span></h6>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <textarea class="form-control" name="reviewer_note" rows="5"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-12 mb-3">
+                                        <h6 class="mb-2 fancy-head"><span>Photography and/or Video Release</span></h6>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <p>Photography and/or Video Release of Participant involved in any activities offered by EMPIRE FIT CLUB may be photographed or videotaped during training. The undersigned hereby consents to the use of these photographs and/or videos without compensation, on the EMPIRE FIT CLUB website or in any editorial, promotional or marketing material produced and/or published by EMPIRE FIT CLUB.</p>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-4">
+                                    <div class="col-12 mb-3">
+                                        <h6 class="mb-2 fancy-head"><span>Waiver and Release of Liability</span></h6>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <ul class="list" style="list-style:none">
+                                            <li><strong>(i)</strong> Express Assumption of Risk: I, the undersigned, am aware that there are significant risks involved in all aspects of physical training. These risks include, but are not limited to: falls, muscle strains, muscle pulls,  muscle tears,  broken  bones,  shin  splints,  heat  prostration,  injuries  to  knees; , injuries to back, injuries to foot, heart attacks, or any other illness or soreness  that I may  incur  which  can  result  in serious injury or death; injury or death  due to negligence  on  the  part of myself,  my training  partner, or other people around me; injury or death due to improper use or failure of equipment; strains and sprains. I am aware that any of these above mentioned risks may result in serious injury or death to myself and or my partner(s). I willingly assume full responsibility for  the  risks that I am exposing myself to and accept full responsibility for  any injury or death that may result from participation  in any activity or class while at, or under direction of EMPIRE FIT CLUB. I acknowledge that I have no physical impairments, injuries , or illnesses that will endanger me or others.</li>
+
+                                            <li><strong>(ii)</strong> Release: In consideration of the above  mentioned  risks  and  hazards  and  in  consideration  of  the  fact that I am willingly and voluntarily participating in the activities offered by EMPIRE FIT CLUB, I,  the undersigned hereby release EMPIRE FIT CLUB,  their  directors,  coaches,  officers,  employees,  agents, from any and all liability, claims, demands,  actions or  rights  of  action, which are related  to,  arise  out  of,  or are in any way connected  with  my participation in this activity,  including  those  allegedly  attributed  to the negligent acts or omissions of the  above mentioned  parties . This  agreement  shall be binding  upon me, my spouse, my children, my guardians, my successors, representatives, heirs, executors, assigns, or transferees (collectively, the "Releasors"). I accept full and  complete  responsibility  for  the  safety  of myself, any guests, observers or other individuals who I have  invited  to  the  venue of  EMPIRE  FIT CLUB, and property, and I assume the risk of  damage, theft, loss  or  injury caused  by others  to  me,  my guests and property <br><br>
+
+                                            If any portion of this agreement is held invalid, I agree that  the  remainder  of  the  agreement  shall remain in full legal force and effect. If I am signing on behalf of a minor child, I also give full permission for any person connected with EMPIRE FIT CLUB to administer first aid deemed necessary, and in case of serious illness or injury, I give permission to call for medical and or surgical care for the child and to transport the child to a medical facility deemed necessary for the wellbeing of the child </li>
+
+                                            <li><strong>(iii)</strong> Indemnification: On behalf of the Releasers, the Participant recognizes that there is risk involved in the types of activities offered by EMPIRE FIT CLUB. Therefore the Participant accepts financial responsibility for any injury that the Participant may cause either to him/herself or to any other participant due to his/her negligence. Should the above mentioned parties, or anyone acting on their behalf, be required to incur attorney's fees and costs to enforce this agreement, I agree to reimburse them for such fees and costs. I further agree to indemnify and hold harmless EMPIRE FIT CLUB, their directors, coaches, officers, employees, agents from liability for the injury or death of any person(s) and damage to property that may result from my negligent or intentional act or omission while participating in <br>
+                                            _____ activities offered by EMPIRE FIT CLUB, at the main building or aboard. This includes but is not limited to parks, recreational areas, playgrounds, areas adjacent to main building, and/or any area selected for training by EMPIRE FIT CLUB. <br><br>
+
+                                            I Have read and understood the foregoing assumption of risk, and release of liability and I understand that by signing it obligates me to indemnify the parties named for any liability for injury or death of any person and damage to property caused by my negligent or intentional act or omission. I understand that by signing this form I am waiving valuable legal rights.</li>
+
+                                            <li><strong>(iv)</strong> Promise Not To Bring Suit: I hereby agree and covenant not to, and shall cause the Releasors not to, bring a claim against, sue, demand compensation from or attach the property or assets of EMPIRE FIT CLUB or any of their directors, coaches, officers, employees, agents, for any loss or damage arising or resulting from my participation in EMPIRE FIT CLUB or my travel to or from or presence at the venue, 2nd forever release and discharge EMPIRE FIT CLUB or any of their directors, coaches, officers, employees, agents them from liability under such claims.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row text-right mt-4">
+                            <div class="col-12">
+                                <button class="btn btn-primary btn-lg">SUBMIT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script>
+    var $  = jQuery;
+    var api = 'http://45.118.132.77/api/health_questions';
+
+    $.ajax( {
+        url: api,
+        success: function ( res ) {
+            //console.log(data);
+            var questions = res['data'];
+            var n = 0;
+            $.each(questions, function(i, item) {
+                console.log(item);
+                var markup = '<label for="question-'+item.id+'" class="d-block mb-4"><input type="checkbox" name="questions_'+item.id+'" id="question-'+item.id+'" class="mr-3"> '+item.question+'</label>';
+
+                $('.questions').append(markup);
+                
+            });
+        }
+    } );
+</script>
+</body>
+</html>
---
+++ member-registration/submit.php
@@ -0,0 +1,53 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Empire Fit | Online Registration</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    
+    <header class="py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-1 col-2">
+                    <img src="images/logo.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-11 col-10 text-right">
+                    <h3>Thank you</h3>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body py-5 px-5">
+                            <div class="text-medium text-center alert alert-success py-4 mb-5" style="font-size: 22px">
+                                WE HAVE RECEIVED YOUR DATA, PLEASE COME TO OUR  CLUB AND TELL YOUR NAME OR PHONE NUMBER TO OUR FRONT OFFICER TO CONTINUE WITH THE MEMBERSHIP PLAN
+                            </div>
+
+                            <div class="fancy-head"><span>Location</span></div>
+
+                            <div class="embed-responsive embed-responsive-21by9">
+                                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15865.47013236382!2d106.82032!3d-6.215166!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7c4cca17a3b48b14!2sEmpire+Fit+Club!5e0!3m2!1sen!2sid!4v1553514098734" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
+
+                            <div class="mt-4 text-right">
+                                <a href="https://empirefitclub.com/" class="btn btn-primary btn-lg">Go to Website</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
+
