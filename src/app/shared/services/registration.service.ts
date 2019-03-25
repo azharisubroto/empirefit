99431ebc69ad13360b0f5f9e7d3351c08b30408c
Riza Alif Wildani <rizaalifdmc@gmail.com>
2019-03-25 12:34:41 +0700

    Fixed staff

---
+++ src/app/shared/services/registration.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { LocalStoreService } from "./local-store.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class RegistrationService {
+  readonly apiURL = "http://45.118.132.77/api";
+  // readonly apiURL = "http://localhost/efc/api";
+
+  constructor(private http: HttpClient, private store: LocalStoreService) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: "Bearer " + this.store.getItem("access_token")
+    })
+  };
+
+  getDevice() {}
+}
