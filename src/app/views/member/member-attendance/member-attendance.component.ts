13d7227863339b1ef7995baab2204ad8ed950a92
Riza Alif Wildani <rizaalifdmc@gmail.com>
2019-04-12 10:10:57 +0700

    Build prod

--- src/app/views/member/member-attendance/member-attendance.component.ts
+++ src/app/views/member/member-attendance/member-attendance.component.ts
@@ -177,7 +177,6 @@ export class MemberAttendanceComponent implements OnInit {
             $("#btn-history").addClass("disabled");
             $("#btn-autoreg").addClass("disabled");
 
-            $("#btn-classhis").addClass("disabled");
             $("#btn-ptsession").addClass("disabled");
             $("#btn-membership").addClass("disabled");
             $("#btn-membership-leave").addClass("disabled");
