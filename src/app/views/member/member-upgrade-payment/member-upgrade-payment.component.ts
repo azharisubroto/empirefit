e55ae95fc4dec417600506c8fa6c61cca337fc99
Azhari Subroto <juarathemes@gmail.com>
2019-04-26 13:49:48 +0700

    more component

--- src/app/views/member/member-upgrade-payment/member-upgrade-payment.component.ts
+++ src/app/views/member/member-upgrade-payment/member-upgrade-payment.component.ts
@@ -94,8 +94,8 @@ export class MemberUpgradePaymentComponent implements OnInit {
     "minWidth": 1,
     penColor: 'rgb(0,0,0)',
     backgroundColor: '#f5f5f5',
-    canvasWidth: 1150,
-    canvasHeight: 550
+    canvasWidth: 1250,
+    canvasHeight: 650
   }
 
   constructor(
